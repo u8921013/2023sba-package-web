c86c7f4fa3570b3450a17e7df5c9e2579898e53a
CSLin <cs.lin@ubn.net>
2025-06-17 09:46:04 +0800

    first commit

---
+++ src/main/java/net/ubn/td/PackageWebApplication.java
@@ -0,0 +1,13 @@
+package net.ubn.td;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PackageWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PackageWebApplication.class, args);
+    }
+}
